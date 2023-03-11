9ce669429ba8c8089462d5aab75ab96d470e9879
Sirius Black <mariia.beliaev@gmail.com>
2023-03-11 12:41:15 +0300

    added task 7

--- task07.js
+++ task07.js
@@ -6,3 +6,20 @@
 В зависимости от оператора программа должна совершать разные операции: складывать первое число со вторым, вычитать из первого числа второе, умножать первое на второе, делить первое на второе.
 Не забудь сделать из строк с числами настоящие числа.
 */
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+const calculate = (firstNumber, secondNumber, operator) => {
+  if (operator === "+") {
+    return +firstNumber + +secondNumber;
+  } else if (operator === "-") {
+    return +firstNumber - +secondNumber;
+  } else if (operator === "*") {
+    return +firstNumber * +secondNumber;
+  } else if (operator === "/") {
+    return +firstNumber / +secondNumber;
+  }
+};
+console.log(calculate("10", "3", "-"));
